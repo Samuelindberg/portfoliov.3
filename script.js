aed13512af51ba7e2155d1f51313ff3df8b6d5aa
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-13 21:13:16 +0100

    work sektion optimiserad, men sidan sliter fortfarande på minnet

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const nameTitle = document.getElementById("name");
 const profileImg = document.getElementById("profile-img");
 const introDesc = document.getElementById("introduction");
 const workTitle = document.getElementById("work-title");
+const myrorna = document.getElementById("myrorna");
+const cicada = document.getElementById("cicada");
 
 window.addEventListener("scroll",()=>{
 nameScrollposition();  
 });
-
 function nameScrollposition(){
    let posY = window.pageYOffset;
-   introDesc.style.opacity=1-posY/800;
-   profileImg.style.transform="translate(0,"+ posY/4 +"px)";
-   introDesc.style.transform="translate("+posY/2+"px,0px)";
-   
 
+   if(posY<1095){
+    profileImg.style.transform="translate(0,"+ posY/4 +"px)";
+    introDesc.style.transform="translate("+posY/2+"px,0px)";
+    introDesc.style.opacity=1-posY/800;
+   } 
+  
   if(posY>1000){
     workTitle.style.transform = "translate(0,"+ (posY-1000) +"px)";
   }
@@ -21,11 +24,16 @@ function nameScrollposition(){
     nameTitle.style.transform="translate(0,"+ posY/1.6 +"px)";
   }
 
-
-
-
-
-   console.log(posY);
-
-
+  if(posY>1395 && posY<1804){
+    myrorna.style.transform = "translate(0,"+ (posY-1395) +"px) scale("+(100+(posY-1395)/20)+"%)";
+    workTitle.innerHTML="MYRORNA VINTAGE";
+  } else if(posY>1892){
+    workTitle.innerHTML="WORKS";
+  }
+  if(posY>3269){
+    workTitle.innerHTML="VEGAN CICADA";
+    workTitle.style.color="#CBF151";
+    cicada.style.transform = "translate(0,"+ (posY-3269) +"px) scale("+(100+(posY-3269)/20)+"%)";
+  }
+  console.log(posY);
 }
