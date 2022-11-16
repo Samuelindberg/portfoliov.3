44aead6973ac803141bac7ba70af68097f802989
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-16 16:14:07 +0100

    parallax fungerar, beskrivning rör sig horisontellt, men hitills också vertikalt

--- script.js
+++ script.js
@@ -1,52 +1,21 @@
-// const nameTitle = document.getElementById("name");
-// const profileImg = document.getElementById("profile-img");
-// const introDesc = document.getElementById("introduction");
-// const workTitle = document.getElementById("work-title");
-// const myrorna = document.getElementById("myrorna");
-// const cicada = document.getElementById("cicada");
-// const tiktactoe = document.getElementById("tiktactoe");
 
-// window.addEventListener("scroll",()=>{
-// nameScrollposition();  
-// });
-// function nameScrollposition(){
-//    let posY = window.pageYOffset;
-// if(posY<1095){
-//   firstPageScroll();
+const introduction = document.getElementById("description");
+const wrapper = document.querySelector(".wrapper");
+
+// window.addEventListener(scroll,(event)=>{
+//     console.log("test");
+//     // let posY = window.pageYOffset;
+//     // introduction.style.color="blue";
+    
 // }
+// );  
+wrapper.addEventListener("scroll",()=>{
+    console.log(wrapper.scrollTop);
+    introduction.style.transform="translate("+wrapper.scrollTop+"px,0px)";
+});
 
-//   if(posY>1000){
-//     workTitle.style.transform = "translate(0,"+ (posY-1000) +"px)";
-//   }
-//   else{
-//     nameTitle.style.transform="translate(0,"+ posY/1.6 +"px)";
-//   }
-
-//   if(posY>1395 && posY<1580){
-//     myrorna.style.transform = "translate(0,"+ (posY-1395) +"px) scale("+(100+(posY-1395)/20)+"%)";
-//     workTitle.innerHTML="MYRORNA VINTAGE";
-//   } else if(posY>1580){
-//     workTitle.innerHTML="WORKS";
-//   }
-//   if(posY>3257 && posY<3444){
-//     workTitle.innerHTML="VEGAN CICADA";
-//     workTitle.style.color="#CBF151";
-//     cicada.style.transform = "translate(0,"+ (posY-3257) +"px) scale("+(100+(posY-3257)/20)+"%)";
-//   } else if(posY>3444){
-//     workTitle.style.color="white";
-//     cicada.style.transform="translate(-"+((posY-3444)*2)+" px,)";
-//   }
-//   if(posY>4319.5 && posY<4467){
-//     tiktactoe.style.transform = "translate(0,"+ (posY-4319.5) +"px) scale("+(100+(posY-4319.5)/20)+"%)";
-//     workTitle.innerHTML="TIC TAC TOE";
-//   }
-//   console.log(posY);
-// }
-
-// function firstPageScroll(){
-//   let posY = window.pageYOffset;
-//     profileImg.style.transform="translate(0,"+ posY/4 +"px)";
-//     introDesc.style.transform="translate("+posY/2+"px,0px)";
-//     introDesc.style.opacity=1-posY/800;
-// }
+function scrollHor(){
+  
+    console.log("test");
+}
 
