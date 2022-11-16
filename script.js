754fe254b3d4f820f35b4376aff3fba25fb77a38
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-16 18:10:58 +0100

    myrorna och work-titel klar

--- script.js
+++ script.js
@@ -8,15 +8,12 @@ wrapper.addEventListener("scroll",()=>{
     let posY = wrapper.scrollTop;
   
     if(wrapper.scrollTop<1123){
-    //    introductionMovement(posY);
     introduction.style.transform="translate("+posY+"px,"+posY/1.5+"px)";
     introduction.style.opacity=1-wrapper.scrollTop/1000;
     }
-    if(posY>350){
+    if(posY>586 && 1700>posY){
      workTitlePosition(posY);
     } 
-    else{
-    }
 });
 
 function introductionMovement(posY){ 
@@ -24,5 +21,9 @@ function introductionMovement(posY){
 introduction.style.opacity=1-wrapper.scrollTop/1000;
 }
 function workTitlePosition(posY){
-   workTitle.style.transform="translate("+(posY-350)+"px,"+(posY/1.3-350)+"px)";
+        workTitle.style.transform="translate("+(posY-586.5)+"px,"+(posY/1.2-586.5)+"px)";
+        profilePic.style.opacity=1-(posY-350)/1000;
+        if(posY<586){
+            profilePic.style.opacity=1
+        }
 }
