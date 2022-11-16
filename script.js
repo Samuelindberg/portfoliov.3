ced5658449ae7e8ad5d34294d6b3fd17e8382f8e
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-16 17:30:23 +0100

    förstasida klar, och work-titel nästan klar

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 
 const introduction = document.getElementById("description");
 const wrapper = document.querySelector(".wrapper");
-const workTitle = document.getElementById
-
-// window.addEventListener(scroll,(event)=>{
-//     console.log("test");
-//     // let posY = window.pageYOffset;
-//     // introduction.style.color="blue";
-    
-// }
-// );  
+const workTitle = document.getElementById("work-title");
+const profilePic = document.getElementById("profile-img");
 wrapper.addEventListener("scroll",()=>{
- 
-    introduction.style.transform="translate("+wrapper.scrollTop+"px,"+wrapper.scrollTop+"px)";
+    console.log(wrapper.scrollTop);
+    let posY = wrapper.scrollTop;
+  
+    if(wrapper.scrollTop<1123){
+    //    introductionMovement(posY);
+    introduction.style.transform="translate("+posY+"px,"+posY/1.5+"px)";
+    introduction.style.opacity=1-wrapper.scrollTop/1000;
+    }
+    if(posY>350){
+     workTitlePosition(posY);
+    } 
+    else{
+    }
 });
 
-function scrollHor(){
-  
-    console.log("test");
+function introductionMovement(posY){ 
+    introduction.style.transform="translate("+posY+"px,"+posY/1.5+"px)";
+introduction.style.opacity=1-wrapper.scrollTop/1000;
 }
-
+function workTitlePosition(posY){
+   workTitle.style.transform="translate("+(posY-350)+"px,"+(posY/1.3-350)+"px)";
+}
